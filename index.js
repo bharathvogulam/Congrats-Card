b5d9fb7373c7aa03107ab550ceeca3cda470b979
bharathreddy <bharathreddyvogulam99@gmail.com>
2022-11-09 11:42:36 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  <div class="bg-container">
+    <h1>Congratulations</h1>
+    <div class="bg-card">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        class="profile"
+      />
+      <h2>Kiran</h2>
+      <p>Vishnu Institute of Computer</p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        class="watch"
+      />
+    </div>
+  </div>
+  // Write your code here.
+);
+
+ReactDOM.render(element, document.getElementById("root"));
